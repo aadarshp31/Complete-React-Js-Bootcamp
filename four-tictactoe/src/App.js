311b1635ede6b30e4c51d20a2a0e90f8bc045ad9
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-24 23:04:07 +0530

    Configured 'react-toastify', 'reactstrap' and basic states for the UI.

--- four-tictactoe/src/App.js
+++ four-tictactoe/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Icon from "./components/Icon";
 
@@ -10,13 +10,32 @@ import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 
+//TicTacToe Grid
+const itemArray = new Array(9).fill("empty");
+
 const App = () => {
+	//React Hooks
+	const [isCross, setIsCross] = useState(false);
+	const [winMessage, setWinMessage] = useState("");
+
+	//Methods
+	const reloadGame = () => {
+		//
+	}
+	const checkIsWinner = () => {
+		//
+	}
+	const changeItem = () => {
+		//
+	}
+
+
 	return (
 		<div className="App">
 			<h1>TIC TAC TOE</h1>
 			<Icon />
 		</div>
 	);
-}
+};
 
 export default App;
