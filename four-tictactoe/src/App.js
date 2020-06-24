a51272b23da2e513904d99c61a24d84acb06c3bb
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-25 00:39:04 +0530

    Created the UI of the Game and added methods.
    Added method to 'realodGame' and 'changeItem' in the grid.
    Added 'winMessage' with 'Play Again' button in case 'winMessage' is set.
    Added display for Whose 'TURN' it is to play.
    Used good color combination to make the app look attractive.

--- four-tictactoe/src/App.js
+++ four-tictactoe/src/App.js
@@ -1,13 +1,15 @@
-import React, {useState} from "react";
-import "./App.css";
+import React, { useState } from "react";
 import Icon from "./components/Icon";
+
+//Stylesheets
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 //React-toastify imports
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 //Reactstrap imports
-import "bootstrap/dist/css/bootstrap.css";
 import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 
 //TicTacToe Grid
@@ -20,21 +22,56 @@ const App = () => {
 
 	//Methods
 	const reloadGame = () => {
-		//
-	}
+		setIsCross(false);
+		setWinMessage("");
+		itemArray.fill("empty", 0, 9);
+	};
 	const checkIsWinner = () => {
 		//
-	}
-	const changeItem = () => {
-		//
-	}
-
+	};
+	const changeItem = (itemNumber) => {
+		if (winMessage) {
+			return toast(winMessage, { type: "success" });
+		}
+		if (itemArray[itemNumber] === "empty") {
+			itemArray[itemNumber] = isCross ? "cross" : "circle";
+			setIsCross(!isCross);
+		} else {
+			return toast("This position is already occupied!", { type: "error" });
+		}
+		checkIsWinner();
+	};
 
 	return (
-		<div className="App">
-			<h1>TIC TAC TOE</h1>
-			<Icon />
-		</div>
+		<Container className="p-5">
+			<ToastContainer position="bottom-center" />
+			<Row>
+				<Col md={6} className="offset-md-3">
+					<h1 className="text-center mb-5">TicTacToe</h1>
+					{winMessage ? (
+						<div className="my-2 text-center text-primary">
+							<h1>{winMessage}</h1>
+							<Button color="success" block onClick={reloadGame}>
+								Play Again
+							</Button>
+						</div>
+					) : (
+						<h3 className="text-center text-danger">
+							TURN: {isCross ? "Cross" : "Circle"}
+						</h3>
+					)}
+					<div className="grid">
+						{itemArray.map((item, index) => (
+							<Card key={index}>
+								<CardBody className="box">
+									<Icon name={item} />
+								</CardBody>
+							</Card>
+						))}
+					</div>
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 
