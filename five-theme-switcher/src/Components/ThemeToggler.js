e81be082e3804acc92e7042f7dd50e73d77db0c9
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-04 23:18:30 +0530

    Bug-fix: onCLick event was directly firing the setThemeMode hook inn ThemeToggler which created an infinite loop.
    Solution: calling this method using a callback so it doesn't crash the app.

--- five-theme-switcher/src/Components/ThemeToggler.js
+++ five-theme-switcher/src/Components/ThemeToggler.js
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
-import ThemeContext from "../Context/ThemeContext"
+import ThemeContext from "../Context/ThemeContext";
 
 const ThemeToggler = () => {
-    const [themeMode, setThemeMode] = useContext(ThemeContext);
-    return(
-        <div
-            onClick={themeMode === "light" ? setThemeMode("dark") : setThemeMode("light")}
-        >
-           <span>{themeMode === "light" ? "Light Mode ON" : "Dark Mode ON"}</span> 
-        </div>
-    );
-}
+	const [themeMode, setThemeMode] = useContext(ThemeContext);
+	return (
+		<div
+			onClick={() => {
+					themeMode === "light" ? setThemeMode("dark") : setThemeMode("light")
+			}}
+		>
+			<span>{themeMode === "light" ? "Light Mode ON" : "Dark Mode ON"}</span>
+		</div>
+	);
+};
 
 export default ThemeToggler;
