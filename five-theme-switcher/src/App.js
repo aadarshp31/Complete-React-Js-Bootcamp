1f9cab541eeb66fa3f948dd98d7ea4e716b59ffc
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-04 23:24:13 +0530

    Added all the components under ThemeContext.Provider
    All the states provided by the context api is available for all enclosed components.
    Header and HeroSection will be able to use states provided by the context api.

--- five-theme-switcher/src/App.js
+++ five-theme-switcher/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import HeroSection from "./Components/HeroSection";
+import Header from "./Components/Header";
+import ThemeContext from "./Context/ThemeContext";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	const themeHook = useState("light");
+	return (
+		<ThemeContext.Provider value={themeHook}>
+			<div>
+				<Header />
+				<HeroSection />
+			</div>
+		</ThemeContext.Provider>
+	);
 }
 
 export default App;
