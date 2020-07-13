4eee8837b7520f0774e4b714e4dd1f9c454a3dd9
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-13 23:03:22 +0530

    Created a section to change it's theme.
    Using 'themeMode' from the context provider to get the JSX styling object from 'AppTheme' component.

---
+++ projects/theme-switcher-app-self-try/src/Components/HeroSection.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import ThemeContext from "../Context/ThemeContext";
+import AppTheme from "../theme/AppTheme";
+
+const HeroSection = (props) => {
+	const [themeMode] = useContext(ThemeContext);
+	const currentTheme = AppTheme[themeMode];
+	return (
+		<div
+			style={{
+				backgroundColor: currentTheme.backgroundColor,
+				color: currentTheme.color,
+			}}
+		>
+			<h3>Context API Theme Toggler</h3>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum sequi
+				sint vel voluptates, nisi cupiditate expedita ex nesciunt officia
+				pariatur laudantium maiores explicabo a neque earum excepturi inventore
+				modi suscipit?
+			</p>
+            <button>Click me</button>
+			{props.children}
+		</div>
+	);
+};
+
+export default HeroSection;
