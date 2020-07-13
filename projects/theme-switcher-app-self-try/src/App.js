125027d2e5d62adfa9d9620d6cb822dbd1db080f
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-13 23:07:01 +0530

    Using Context API to provide 'themeMode' as a global state.
    Fetched default value from the 'ThemeContext' to use as defaut state 'themeMode'.
    '[themeMode, setThemeMode]' is a hook that is passed as a value in the Provider component for ThemeContext.

--- projects/theme-switcher-app-self-try/src/App.js
+++ projects/theme-switcher-app-self-try/src/App.js
@@ -1,9 +1,21 @@
-import React from "react"
-import "./App.css"
+import React, { useContext, useState, Fragment } from "react";
+import "./App.css";
+import ThemeContext from "./Context/ThemeContext";
+import HeroSection from "./Components/HeroSection";
+import ThemeToggler from "./Components/ThemeToggler";
 const App = () => {
-    return(
-        <h1>Theme Swither App</h1>
-    );
-}
+	const theme = useContext(ThemeContext);
+	const [themeMode, setThemeMode] = useState(theme); //Acting as a Global State
+	return (
+		<ThemeContext.Provider value={[themeMode, setThemeMode]}>
+			<Fragment>
+				<h1>Theme Switcher App</h1>
+				<HeroSection>
+					<ThemeToggler />
+				</HeroSection>
+			</Fragment>
+		</ThemeContext.Provider>
+	);
+};
 
 export default App;
