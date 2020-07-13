11118dc8d1d8c1481842946466941b6e2acca0ac
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-13 23:00:14 +0530

    Created a Theme object written in JSX based CSS.

---
+++ projects/theme-switcher-app-self-try/src/theme/AppTheme.js
@@ -0,0 +1,12 @@
+const AppTheme = {
+        light: {
+            backgroundColor: "#ffffff",
+            color: "#000000"
+        },
+        dark: {
+            backgroundColor: "#333333",
+            color: "#ffffff"
+        }
+}
+
+export default AppTheme;
