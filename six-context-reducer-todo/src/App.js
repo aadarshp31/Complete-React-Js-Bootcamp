18230639c0a6f7ac99a730b3018c8f0f7f90a8f3
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-16 22:12:50 +0530

    Improved the layout of the project with proper spacing, positioning and minimal usage of box shadows.

--- six-context-reducer-todo/src/App.js
+++ six-context-reducer-todo/src/App.js
@@ -13,8 +13,8 @@ const App = () => {
     <TodoContext.Provider value={{todos, dispatch}}>
       <Container fluid>
         <h1>TODO app with Context API and Reducer</h1>
-        <Todos />
         <TodoForm />
+        <Todos />
       </Container>
     </TodoContext.Provider>
   );
