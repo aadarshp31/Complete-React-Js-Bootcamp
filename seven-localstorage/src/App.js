2f4329171e1a19b2eb7ad5a98e85c853e4ce086a
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-17 23:45:24 +0530

    Added 'Todos' and 'TodoForm' components to the App.js and removed any errors present in them.

--- seven-localstorage/src/App.js
+++ seven-localstorage/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {Container} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const localTodos = localStorage.getItem("todos");
+    if(localTodos){
+      setTodos(JSON.parse(localTodos));
+    }
+  }, [])
+
   return (
-    <div></div>
+    <Container type="fluid">
+      <h3>TODO app with Context API and Reducer</h3>
+        <TodoForm addTodos={addTodos}/>
+        <Todos todos={todos} markComplete={markComplete} />
+    </Container>
   );
 }
 
