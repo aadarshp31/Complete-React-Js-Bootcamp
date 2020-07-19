0def33c1703b17cb426659acf9cc910175a8f45a
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-20 00:39:06 +0530

    Rendering API json data into a Card component successfully.

--- eight-random-user-generator/src/Card.js
+++ eight-random-user-generator/src/Card.js
@@ -1,19 +1,57 @@
 import React from "react";
-import { Card, CardBody, CardTitle, CardText } from "reactstrap";
-import { FaEnvelope, FaMapMarked, FaPhone } from "react-icons/fa";
+import { Card, CardBody, CardTitle, CardFooter } from "reactstrap";
+import { FaEnvelope, FaMapMarked, FaPhone, FaUser } from "react-icons/fa";
 
-const MyCard = ({ details }) => (
-    <Card>
-        <CardBody className="text-center">
-            <img
-                src={details.picture?.large} 
-                height="150" 
-                width="150" 
-                className="rounded-circle img-thumbnail border-danger" 
-                alt="User Profile Picture" 
+const MyCard = ({ details }) => {
+    const DOB = new Date(details.dob?.date).toLocaleString().split(",")[0];
+    return (
+        <Card>
+            <CardBody className="text-center">
+                <img
+                    src={details.picture?.large}
+                    height="150"
+                    width="150"
+                    className="rounded-circle img-thumbnail border-danger"
+                    alt="User Profile"
                 />
-        </CardBody>
-    </Card>
-)
+                <CardTitle className="text-primary">
+                    <h1>
+                        <span>{details.name?.title + " "}</span>
+                        <span>{details.name?.first + " "}</span>
+                        <span>{details.name?.last}</span>
+                    </h1>
+                </CardTitle>
+                    <h5><FaEnvelope />{" " + details.email}</h5>
+                    <h6>
+                        <FaMapMarked />
+                        {" " + details.location?.city + ", "}
+                        {details.location?.state}
+                    </h6>
+                    <h6>
+                        <FaUser />
+                    username:
+                    {" " + details.login?.username}
+                    </h6>
+                    <h6>
+                        DOB:
+                        
+                            {" " + DOB}
+                    </h6>
+                    <h6>
+                        Age:
+                        <span className="badge-dark ml-2 p-1 rounded">
+                            {details.dob?.age}
+                        </span>
+                    </h6>
+            </CardBody>
+            <CardFooter>
+                <h6>
+                    <FaPhone />
+                    <span>{details.phone}</span>
+                </h6>
+            </CardFooter>
+        </Card>
+    );
+}
 
 export default MyCard;
