883ec0509291b0dadb7018d36b1530827181fe36
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-19 22:45:35 +0530

    Fired a 'Get' request to an api by using 'Axios' and stored the detials in state using 'Effect' hook.

--- eight-random-user-generator/src/App.js
+++ eight-random-user-generator/src/App.js
@@ -1,10 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Container, Row, Col } from "reactstrap";
 import './App.css';
 import Axios from "axios";
 
 function App() {
+  const [details, setDetails] = useState({});
+
+  const fetchDetails = async () => {
+    const { data } = await Axios.get('https://randomuser.me/api/');
+    const details = data.results[0];
+    setDetails(details);
+  }
+
+  useEffect(() => {
+    fetchDetails();
+  }, [])
+
+
   return (
     <div className="App">
       
