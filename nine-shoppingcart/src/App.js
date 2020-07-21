b272b5e84bb56f2e3ae799d6831b2afd039174c3
aadarshp31 <adarshpandey.in@gmail.com>
2020-07-21 22:57:02 +0530

    Created a method to add item to cart.

--- nine-shoppingcart/src/App.js
+++ nine-shoppingcart/src/App.js
@@ -1,11 +1,47 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
+import { toast } from 'react-toastify';
 import './App.css';
 
-const App = () =>  {   
+const App = () => {
   const [cart, setCart] = useState([]);
+
+  // Method to add items to cart
+  const addToCart = item => {
+    // Checking if the item is already present in cart.
+    const isAlreadyInCart = cart.findIndex(function (itemInCart) {
+      return itemInCart.id === item.id;
+    })
+
+    // Show a toast component to specify item already is in cart
+    if (isAlreadyInCart !== -1) {
+      return toast("Item is already present in cart!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        type: "error"
+      })
+    }
+
+    // Add item to cart
+    setCart([...cart, item]);
+  }
+
+  toast("Item is already present in cart!", {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    type: "error"
+  })
 
   return (
     <div className="App">
