4d2f0da73abfec9b5d594ee1711d7947e6188cf4
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-01 20:34:15 +0530

    Fetching and Rendering data from Pexels API.
    Deleted the local api method as it was not necessary.

--- nine-shoppingcart/src/components/PurchasePage.js
+++ nine-shoppingcart/src/components/PurchasePage.js
@@ -1,28 +1,34 @@
-import React, {useState, useEffect} from "react";
-import {Axios} from "axios";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import { random, commerce } from 'faker';
 import { Container, Col, Row } from "reactstrap";
 
 // Setting up entities for making api request
 const apiKey = process.env.REACT_APP_API_KEY;
 const apiUrl = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
-const localUrl = "https://dummyurl.com/api/"
 
-const PurchasePage = ({addToCart}) => {
+const PurchasePage = ({ addToCart }) => {
     const [products, setProducts] = useState([]);
 
     // Fetching photos from pexels api.
     const fetchPhotos = async () => {
-        const response = await Axios.get(apiUrl, {
-            header: {
+        const { data } = await Axios.get(apiUrl, {
+            headers: {
                 Authorization: apiKey
             }
         })
-    }
 
-    // Fetching photos from local api.
-    const fetchPhotosLocal = async () => {
-        const { data } = await Axios.get(localUrl);
+
+        const { photos } = data;
+        const allPhotosLocal = photos.map(photo => ({
+            smallImage: photo.src.medium,
+            tinyImage: photo.src.tiny,
+            productName: random.word(),
+            productPrice: commerce.price(),
+            id: random.uuid()
+        }))
+
+        setProducts(allPhotosLocal);
     }
 
     // Fetching data from api at the time of app load
@@ -30,8 +36,25 @@ const PurchasePage = ({addToCart}) => {
         fetchPhotos()
     }, [])
 
-     
-    return();
+
+    return (
+        <Container fluid>
+            <h1 className="text-info text-center">Buy Page</h1>
+            <Row>
+                <Col>
+                    {products.map(product => (
+                        <div key={product.id}>
+                            <span>{product.productName}</span>
+                            <br />
+                            <span>{product.productPrice}</span>
+                            <br />
+                            <br />
+                        </div>
+                    ))}
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default PurchasePage;
