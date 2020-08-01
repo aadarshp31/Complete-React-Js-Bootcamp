ccbd183c43455f56a25a6a0cce0d83c9c81f5b96
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-01 20:35:44 +0530

    Removed boilerplate react logo and added Purchase pge component.

--- nine-shoppingcart/src/App.js
+++ nine-shoppingcart/src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import PurchasePage from './components/PurchasePage';
 
 const App = () => {
   const [cart, setCart] = useState([]);
@@ -58,20 +59,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PurchasePage addToCart={addToCart} />
     </div>
   );
 }
