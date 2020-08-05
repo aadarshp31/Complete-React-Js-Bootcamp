c7893543242f1d34d23f6e16af5aca9be78d9dba
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-05 20:28:36 +0530

    Imported all dependencies and Components to App.js
    Also created a Context called 'UserContext' .

--- ten-firebase-github-app/src/App.js
+++ ten-firebase-github-app/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Bootstrap css for reactstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// React Router
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+
+// Toast
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/reactToastify.min.css";
+
+// Fireabase
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// Components
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import PageNotFound from "./pages/PageNotFound";
+
+const App = () => {
+
+  // Default state is set to null as firebase Auth requires it to be null to work
+  const [user, SetUser] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
