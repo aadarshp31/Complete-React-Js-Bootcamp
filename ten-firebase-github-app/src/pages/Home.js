9c626859fd3259f7f9f946f737dd9c2ab381bf8c
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-08 23:46:02 +0530

    Added a form and states to handle user fetch from Github API.

--- ten-firebase-github-app/src/pages/Home.js
+++ ten-firebase-github-app/src/pages/Home.js
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { useState, useContext } from "react";
+import Axios from "axios";
+
+import {
+  Row,
+  Container,
+  Col,
+  Input,
+  Button,
+  InputGroup,
+  InputGroupAddon,
+} from "reactstrap";
+
+import UserCard from "../components/UserCard";
+import Repos from "../components/Repos";
+import { Redirect } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+import { toast } from "react-toastify";
 
 const Home = () => {
-    return(
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-}
+  const context = useContext(UserContext);
+  const [query, setQuery] = useState("");
+  const [user, setUser] = useState(null);
+
+  const fetchGithubUser = async () => {
+    try {
+      const { data } = Axios.get(`https://api.github.com/users/${query}`);
+      setUser(data);
+    } catch (err) {
+      toast(err.message, {
+        type: "error",
+      });
+    }
+  };
+
+  const handleSubmit = () => {
+    fetchGithubUser();
+  }
+
+  return (
+    <Container>
+      <Row className=" mt-3">
+        <Col md="5">
+          <InputGroup>
+            <Input
+              type="text"
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+              placeholder="Please provide the username"
+              autoFocus
+              required
+            />
+            <InputGroupAddon addonType="append">
+              <Button color="primary">Fetch User</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Col>
+        <Col md="7"></Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Home;
