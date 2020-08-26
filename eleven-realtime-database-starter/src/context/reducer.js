2f7b0ebf977715387a8d4102a0532bcf51c95b4a
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-26 20:13:18 +0530

    Created the Switch case to handle 'action types'.
    Also removed the 'TODO:' badges from the 'reducer' file and the 'Context' file.

--- eleven-realtime-database-starter/src/context/reducer.js
+++ eleven-realtime-database-starter/src/context/reducer.js
@@ -1,5 +1,3 @@
-//TODO: create contact using all actions
-
 import {
   SET_CONTACT,
   SET_LOADING,
@@ -7,4 +5,27 @@ import {
   SET_SINGLE_CONTACT
 } from "./action.types";
 
-//TODO: use switch case
+export default (state, action) => {
+  switch(action.type){
+    case SET_CONTACT:
+      return action.payload === null ? {...state, contacts: []
+      } : {
+        ...state, contacts: action.payload
+      };
+    case SET_LOADING:
+      return {...state, isLoading: action.payload}
+    case CONTACT_TO_UPDATE:
+      return {
+        ...state,
+        contactToUpdate: action.payload,
+        contactToUpdateKey: action.key
+      }
+    case SET_SINGLE_CONTACT:
+      return {
+        ...state,
+        contact: action.payload
+      }
+    default:
+      return state
+  }
+}
