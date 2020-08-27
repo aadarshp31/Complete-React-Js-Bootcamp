dec8073562cdabcd9c40484d014194e1bff30f85
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-27 20:01:06 +0530

    Bug-fix: The firebase config was not able to load directly from env as an object.
    Made individual env variables for various sensitive firebase credentials.

--- eleven-realtime-database-starter/src/utils/config.js
+++ eleven-realtime-database-starter/src/utils/config.js
@@ -1,5 +1,14 @@
 // firebase config
-export const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG;
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: `${process.env.REACT_APP_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.REACT_APP_PROJECT_ID}.firebaseio.com`,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: `${process.env.REACT_APP_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+}
 
 //image configuration
 export const imageConfig = {
