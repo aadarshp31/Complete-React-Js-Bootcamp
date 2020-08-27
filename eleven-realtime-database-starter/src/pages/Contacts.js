e3c5b06dd3615c1fc35fafac5c1e7e3351ccb032
aadarshp31 <adarshpandey.in@gmail.com>
2020-08-27 19:24:05 +0530

    Extracted all contacts from DB and rendering them in a listGroup component.
    Bug-fix: if CONTACT_TO_UPDATE is set and 'AddContact' method is invoked then CONTACT_TO_UPDATE should be set to null to avoid bugs.

--- eleven-realtime-database-starter/src/pages/Contacts.js
+++ eleven-realtime-database-starter/src/pages/Contacts.js
@@ -19,7 +19,12 @@ const Contacts = () => {
   // handle fab icon button click
   // will set in state of the contact to update and send it to the contact/add route
   const AddContact = () => {
-    //TODO: use dispatch to send user to add contact screen
+    // using dispatch to send user to add contact screen
+    dispatch({
+      type: CONTACT_TO_UPDATE,
+      payload: null,
+      key: null
+    })
     history.push("/contact/add");
   };
 
@@ -35,7 +40,14 @@ const Contacts = () => {
 
   return (
     <Container className="mt-4">
-      {/* TODO: Loop through FIREBASE objects  */}
+      {/* Loop through FIREBASE objects  */}
+      <ListGroup>
+        {Object.entries(contacts).map(([key, value]) => (
+          <ListGroupItem key={key}>
+            <Contact contact={value} contactKey={key} />
+          </ListGroupItem>
+        ))}
+      </ListGroup>
       <MdAdd className="fab icon " onClick={AddContact} />
     </Container>
   );
